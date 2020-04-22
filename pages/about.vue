d6d24e87526951ec75c42fa4e5dd9aa0edbd45b6
roarschouten <roarschouten@gmail.com>
2020-04-22 22:50:54 +0200

    Testing API

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,48 @@
 <template>
     <div class="container">
-        <h1>Hello world!</h1>
+        <h1>{{ pagina.paginaTitel }}</h1>
     </div>
 </template>
+
+<script>
+import { request, gql, imageFields, seoMetaTagsFields } from '~/lib/datocms'
+import { toHead } from 'vue-datocms'
+import format from 'date-fns/format'
+import parseISO from 'date-fns/parseISO'
+
+export default {
+  async asyncData({ params }) {
+    const data = await request({
+      query: gql`
+        {
+          site: _site {
+            favicon: faviconMetaTags {
+              ...seoMetaTagsFields
+            }
+          }
+
+         pagina {
+            paginaTitel
+        }
+
+        ${imageFields}
+        ${seoMetaTagsFields}
+      `
+    })
+
+    return { ready: !!data, ...data }
+  },
+  methods: {
+    formatDate(date) {
+      return format(parseISO(date), 'PPP')
+    }
+  },
+  head() {
+    if (!this.ready) {
+      return
+    }
+
+    return toHead(this.site.favicon)
+  }
+}
+</script>
