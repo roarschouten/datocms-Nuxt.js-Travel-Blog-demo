f148a48f938cbed17b3cfc919fcfbf9c962761cc
roarschouten <roarschouten@gmail.com>
2020-04-23 13:31:49 +0200

    Changes about

--- pages/about.vue
+++ pages/about.vue
@@ -1,49 +1,5 @@
 <template>
     <div class="container">
-        <h1>{{ pagina.paginaTitle }}</h1>
+        <h1>{{ post.title }}</h1>
     </div>
 </template>
-
-<script>
-import { request, gql, imageFields, seoMetaTagsFields } from '~/lib/datocms'
-import { toHead } from 'vue-datocms'
-import format from 'date-fns/format'
-import parseISO from 'date-fns/parseISO'
-
-export default {
-  async asyncData({ params }) {
-    const data = await request({
-      query: gql`
-        {
-          site: _site {
-            favicon: faviconMetaTags {
-              ...seoMetaTagsFields
-            }
-          }
-
-         pagina {
-            paginaTitel
-          }
-        }
-
-        ${imageFields}
-        ${seoMetaTagsFields}
-      `
-    })
-
-    return { ready: !!data, ...data }
-  },
-  methods: {
-    formatDate(date) {
-      return format(parseISO(date), 'PPP')
-    }
-  },
-  head() {
-    if (!this.ready) {
-      return
-    }
-
-    return toHead(this.site.favicon)
-  }
-}
-</script>
